b06d0b69b06ef67903785721859830d74d1ff389
Milan Varghese Joseph <125987303+MJitsme@users.noreply.github.com>
2023-08-10 18:26:03 +0530

    Create evaluationcoordinator.jsx

---
+++ client/pages/evaluationcoordinator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/evaluationcoordinator.css";
+
+function EvaluationCoordinator() {
+  return (
+    <div className="mark">
+      <img className="logo" />
+      <div className="line"></div>
+      <p className="usertype">Coordinator's Evaluation</p>
+      <div className="bg_rect"></div>
+    </div>
+  );
+}
+
+export default EvaluationCoordinator;
